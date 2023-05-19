47682d76fe69af31df040e9f212f4734f5214350
Albina <evers.albina@gmail.com>
2023-05-19 12:00:31 +0300

    Edit: added relations between Student and Course classes

---
+++ src/main/java/lv/venta/models/Course.java
@@ -0,0 +1,64 @@
+package lv.venta.models;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lv.venta.models.users.Student;
+
+@Table(name = "course_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Course {
+
+	
+	@Column(name = "Idcourse")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private long idcourse;
+	
+	@Column(name = "Title")
+	@NotNull
+	@Size(min = 3, max = 35)
+	@Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Pirmajam burtam jabut lielajam")
+	private String title;
+	
+	@Column(name = "CreditPoints")
+	@Min(0)
+	@Max(24)
+	private int creditPoints;
+
+	@ManyToMany(mappedBy = "debtCourses")
+	private Collection<Student> debtCourses = new ArrayList<>();
+	
+	
+	public Course(
+			@NotNull @Size(min = 3, max = 35) @Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Pirmajam burtam jabut lielajam") String title,
+			@Min(0) @Max(24) int creditPoints) {
+		this.title = title;
+		this.creditPoints = creditPoints;
+	}
+	
+	
+	
+}
---
+++ src/main/java/lv/venta/models/users/Student.java
@@ -0,0 +1,57 @@
+package lv.venta.models.users;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lv.venta.models.Course;
+
+@Table(name = "student_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Student extends Person {
+
+	//TODO izveidot Data JPA annotacijas
+	//TODO izveidot validaciju annotacijas
+	//TODO izveidot sasaisti ar Course klasi ManyToMany
+	@Column(name = "MatriculaNo")
+	@NotNull
+	//@Size(min = 8, max = 20)
+	@Pattern(regexp = "[0-9]{8,20}")
+	private String matriculaNo;
+	
+	@Column(name = "FinancialDebt")
+	private boolean financialDebt;
+
+	@ManyToMany
+	@JoinTable(name = "student_debt_courses_table", joinColumns = @JoinColumn(name = "Idcourse"), inverseJoinColumns = @JoinColumn(name = "Idperson"))
+	private Collection<Course> debtStudents = new ArrayList<>();
+	
+	public Student(
+			@NotNull @Size(min = 3, max = 15) @Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Pirmajam burtam jabut lielajam") String name,
+			@NotNull @Size(min = 3, max = 15) @Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Pirmajam burtam jabut lielajam") String surname,
+			@NotNull @Size(min = 12, max = 12) @Pattern(regexp = "[0-9]{6}-[0-9]{5}", message = "Pirmajam burtam jabut lielajam") String personcode,
+			User user, @NotNull @Pattern(regexp = "[0-9]{8,20}") String matriculaNo, boolean financialDebt) {
+		super(name, surname, personcode, user);
+		this.matriculaNo = matriculaNo;
+		this.financialDebt = financialDebt;
+	}
+	
+	
+	
+	
+}
