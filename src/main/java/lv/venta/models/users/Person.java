bda18d4662814925e3690648e1951820f15f0863
Albina <evers.albina@gmail.com>
2023-05-19 11:33:27 +0300

    Edit: validations

--- src/main/java/lv/venta/models/users/Person.java
+++ src/main/java/lv/venta/models/users/Person.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToOne;
@@ -23,6 +25,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 //TODO pielikt toString ja nepieciesams
 public class Person {
 	
