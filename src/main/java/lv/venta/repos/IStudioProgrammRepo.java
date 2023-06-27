17e531f7d82cdbc9bbc57e03eeea2c6e0aa92d0f
Albina <evers.albina@gmail.com>
2023-06-27 10:17:13 +0300

    ŅEW: in lv.venta.repos package new class has been added IStudioProgrammRepo

---
+++ src/main/java/lv/venta/repos/IStudioProgrammRepo.java
@@ -0,0 +1,10 @@
+package lv.venta.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.models.StudioProgramm;
+
+
+public interface IStudioProgrammRepo extends CrudRepository<StudioProgramm, Long>{
+
+}
