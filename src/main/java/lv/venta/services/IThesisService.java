30d0cdb767510d3207fb26ccdd5845bcb4a4b1de
Albina <evers.albina@gmail.com>
2023-06-27 18:13:52 +0300

    ADDED: ThesisService functions without functions from IThesisRepo

--- src/main/java/lv/venta/services/IThesisService.java
+++ src/main/java/lv/venta/services/IThesisService.java
@@ -2,6 +2,7 @@ package lv.venta.services;
 
 import java.util.List;
 
+import lv.venta.models.AcceptanceStatus;
 import lv.venta.models.Thesis;
 import lv.venta.models.users.AcademicPersonel;
 import lv.venta.models.users.Student;
@@ -9,9 +10,11 @@ import lv.venta.models.users.Student;
 public interface IThesisService {
 
 	void createThesis(Thesis thesis);
-	void deleteThesis(long idthesis);
+	void deleteThesis(long idthesis) throws Exception;
 	void updateThesis(long idthesis, String titleEn, String titleLv, String goal, String tasks, Student student,
-			AcademicPersonel supervisor) throws Exception;
+			AcademicPersonel supervisor, AcceptanceStatus status) throws Exception;
 	List<Thesis> getAllThesises();
+	
+	//TODO pievienot funkcijas prieks Repo funkcijam
 	
 }
