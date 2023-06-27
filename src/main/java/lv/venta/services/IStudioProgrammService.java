c38d9e815b8809cf62ad13e2d8644a138646f27d
Albina <evers.albina@gmail.com>
2023-06-27 13:58:14 +0300

    ADDED: StudioProgrammService implement with all needed functions

---
+++ src/main/java/lv/venta/services/IStudioProgrammService.java
@@ -0,0 +1,16 @@
+package lv.venta.services;
+
+import java.util.List;
+
+import lv.venta.models.Degree;
+import lv.venta.models.Faculty;
+import lv.venta.models.StudioProgramm;
+
+public interface IStudioProgrammService {
+
+	void createStudioProgramm(StudioProgramm programm);
+	void updateStudioProgramm(long idstprog, Faculty faculty, Degree degree, String title) throws Exception;
+	void deleteStudioProgramm(long idstprog) throws Exception;
+	List<StudioProgramm> getAllStudioProgramms();
+	StudioProgramm getStudioProgrammById(long idstprog);
+}
