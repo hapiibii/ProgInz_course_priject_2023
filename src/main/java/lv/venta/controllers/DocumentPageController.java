7036782e8a557b40ccccfb8b31edb94aedf14f18
Paula <paula.cielava1998@gmail.com>
2023-06-29 22:14:19 +0300

    Edit: document-page and documentPageController. Add: submission-page.html

--- src/main/java/lv/venta/controllers/DocumentPageController.java
+++ src/main/java/lv/venta/controllers/DocumentPageController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lv.venta.models.Documents;
@@ -31,19 +34,47 @@ public class DocumentPageController {
 	}
 	
 	
+	@GetMapping("/all-documents")
+	public String showAllDocuemnts (Model model) {
+		List<Documents> allDocuments = documentService.retrieveAllDocuments();
+		model.addAttribute("allDocuemnts", allDocuments);
+		return "all-documents-page";
+	}
+	
+	@GetMapping("/create-documents")
+	public String showCreate (Model model) {
+		model.addAttribute("document", new Documents());
+		return "document-insert-page";
+	}
+	
+	@PostMapping("/create-documents")
+	public String createDocuments (@ModelAttribute("document") Documents documents) {
+		documentService.insertDocument(documents.getDocumentName(), documents.getFile());
+		return "redirect:/document-page";
+		
+	}
 	
 	
+	@GetMapping("/update-document/{iddocument}")
+	public String updateDocumentGetFunc (@PathVariable("iddocument") long iddocument, Model model) throws Exception {
+		Documents documents = documentService.retrieveDocumentById(iddocument);
+		model.addAttribute("document", documents);
+		return "document-update-page";
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@PostMapping("/update-document/{iddocument}")
+	public String updateDocumentPostFunc (@PathVariable("iddocument") long iddocument, @ModelAttribute("documents") Documents documents ) throws Exception {
+		documentService.updateDocument(iddocument, documents.getDocumentName(), documents.getFile());
+		return "redirect:/all-documents";
+	}
+	/*
+	@GetMapping("/delete-document/{iddocument}")
+	public String deleteDocumentGetFunc (@PathVariable("iddocument") long iddocument, Model model) {
+		Documents documents = documentService.deleteDocumentByDocumetId(iddocument);
+		model.addAttribute("documents", documents);
+		return "document-delete-confirm";
+	}
+	*/
 	
 	
 }
