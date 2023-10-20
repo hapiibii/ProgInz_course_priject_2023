32cefda38a31b6c114d1cb9c0b676c4985503cae
Paula <paula.cielava1998@gmail.com>
2023-10-20 11:08:42 +0300

    Edit: calendarSchadule

--- src/main/java/lv/venta/repos/ICalendarScheduleRepo.java
+++ src/main/java/lv/venta/repos/ICalendarScheduleRepo.java
@@ -9,14 +9,15 @@ import lv.venta.models.CalendarSchedule;
 import lv.venta.models.StudioProgramm;
 
 public interface ICalendarScheduleRepo  extends CrudRepository<CalendarSchedule, Long> {
-	
-	CalendarSchedule findByGadsAndStudioProgramm(int year, StudioProgramm studioProgramm);
+  
 
-	List<CalendarSchedule> findByStudioProgramm(StudioProgramm studioProgramm);
+  List<CalendarSchedule> findByStudioProgramm(StudioProgramm studioProgramm);
 
-	CalendarSchedule findByStudioProgrammAndGads(StudioProgramm studioProgramm, int gads);
-	
-	@Query("SELECT DISTINCT cs.gads FROM CalendarSchedule cs ORDER BY cs.gads DESC")
-	List<Integer> findAllUniqueYears();
-	
+  CalendarSchedule findByStudioProgrammAndGads(StudioProgramm studioProgramm, int gads);
+  
+  @Query("SELECT DISTINCT cs.gads FROM CalendarSchedule cs ORDER BY cs.gads DESC")
+  List<Integer> findAllUniqueYears();
+
+  List<CalendarSchedule> findByGads(int year);
+  
 }
